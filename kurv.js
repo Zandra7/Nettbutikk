0f38d61be13cd79edc572b6f9d5fb76841162a4e
Sandra Elise Eng <sandra.eng@live.no>
2023-05-12 09:44:47 +0200

    Handlekurv fix

--- kurv.js
+++ kurv.js
@@ -1,3 +1,5 @@
+var basket = [];
+
 document.addEventListener("DOMContentLoaded", function() {
     var basketCookie = document.cookie.split(";").filter(function(el) {
         return el.trim().startsWith("basket=");
@@ -9,17 +11,31 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     
     console.log("basketCookie", basketCookie);
+    basket = JSON.parse(basketCookie);
+    
     var handleBeholder = document.getElementsByClassName("handle-beholder");
     for (var i = 0; i < handleBeholder.length; i++) {
         console.log("handleBeholder[i].id", handleBeholder[i].id);
-        // Loop through each child element within the current handle-beholder element
         var children = handleBeholder[i].children;
         for (var j = 0; j < children.length; j++) {
             console.log("handleBeholder[i].children[j].id", children[j].id);
-            if (!basketCookie.includes(children[j].id)) {
+            if (!basket.includes(children[j].id)) {
                 children[j].remove();
-                j--; // Decrement the index to account for the removed element.
+                j--;
             }
         }
-    }
+    } 
 });
+
+function fjernAlt() {
+    console.log("slettAlt")
+    var basket = [];
+    console.log("basket", basket)
+    updateCookies();
+}
+
+function updateCookies() {
+    console.log("updateCookies", basket)
+    document.cookie = "basket=" + encodeURIComponent(JSON.stringify(basket));
+}
+
